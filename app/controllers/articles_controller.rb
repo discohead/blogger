98d1f4d17b1d83f5ac67edcf2b27c34d666007ab
Jared McFarland <jaredcolin@gmail.com>
2014-05-20 12:15:02 -0700

    edit, destroy, update actions/views added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
 
   def index
     @articles = Article.all
@@ -13,7 +14,28 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+    flash.notice = "Article '#{@article.title}' Saved!"
+    redirect_to article_path(@article)
+  end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash.notice = "Article Destroyed!"
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
   end
 
 end
